9f97687f965b79535ff97f3249089ed3fe4f66c7
Vhiper-JM <jmosque17031@universidadean.edu.co>
2021-10-29 14:49:47 -0500

    Acutalizacion ultima version

--- acinoyx_jubatus/auth.py
+++ acinoyx_jubatus/auth.py
@@ -1,11 +1,116 @@
-from flask import Blueprint
-from flask.templating import render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for, make_response
+from werkzeug.wrappers import response
+from .__init__ import *
+from werkzeug.security import generate_password_hash, check_password_hash
+from acinoyx_jubatus import db    
+from flask_login import login_user, login_required, logout_user, current_user       
+import sqlite3 as sql
+from datetime import timezone
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/", methods=["GET" "POST"])
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('correo')
+        password = request.form.get('Contraseña')
+        checkid = request.form.get('checkid')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash(f'Logueado exitosamente como {user.user_name}', category='success')
+                if checkid:
+                    login_user(user, remember=True)
+                    redirect(url_for('views.feed'))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+                    result = db.engine.execute(
+                                                    """
+                                                SELECT photo, foot_note, post_date, photo_user_name
+                                                FROM post
+                                                ORDER
+                                                BY id DESC
+                                                    """
+                                                )
+
+
+                    response = make_response(render_template("feed.html", posts=result, user=current_user))                   
+                    response.set_cookie("user_id",
+                                        f"{user.id}",
+                                        secure=True)
+                    response.set_cookie("photo_user_name",
+                                        f"{user.user_name}",
+                                        secure=True)
+                    return response
+                else:
+                    login_user(user, remember = False)
+                    redirect(url_for('views.feed'))
+                    result = db.engine.execute(
+                                                    """
+                                                SELECT photo, foot_note, post_date, photo_user_name
+                                                FROM post
+                                                ORDER
+                                                BY id DESC
+                                                    """
+                                                )
+
+
+                    response = make_response(render_template("feed.html", posts=result, user=current_user))
+                    redirect(url_for('views.feed'))
+                    response.set_cookie("user_id",
+                                        f"{user.id}",
+                                        secure=True)
+                    response.set_cookie("photo_user_name",
+                                        f"{user.user_name}",
+                                        secure=True)
+                    return response
+            else:
+                flash('Contraseña incorrecta, intentelo de nuevo.', category='error')
+                return render_template("login.html", user=current_user)
+        else:
+            flash('El correo electronico no exite', category='error')
+        return render_template("login.html", user=current_user)
+
+    return render_template("login.html", user=current_user) #Depending on the status of the current_user (signed in or not), the login button is displayed
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Ha salido de su cuenta exitosamente!")
+    return redirect(url_for('auth.login'))
+
+@auth.route("/registro", methods=["GET", "POST"])
 def registro():
-    return render_template("registro.html")
+    if request.method == 'POST':
+        name = request.form['nombre']
+        email = request.form['correo']
+        age = request.form['edad']
+        password = request.form['Contraseña']
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('El correo ya existe', category='error')
+
+        elif len(name) < 2:
+            flash('Su nombre debe tener al menos dos letras.', category='error')
+            pass
+        elif len(email) < 4:
+            flash('Su correo debe ser mayor a 4 caracteres.', category='error')
+            pass
+        elif 18 <= int(age) >= 100:
+            flash('Debe tener almenos 18 años', category='error')
+            pass
+        elif len(password) < 7:
+            flash('Debe tener almenos 18 años', category='error')
+            pass
+        else:   # add user to the database 
+            new_user = User(email=email, user_name=name, age=age, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registro exitoso!', category='succes')
+            return redirect(url_for('auth.login'))
+            
+    return render_template("registro.html", user=current_user)   #Depending on the status of the current_user (signed in or not), the sign up button is displayed
     
 
 
--- acinoyx_jubatus/views.py
+++ acinoyx_jubatus/views.py
@@ -1,59 +1,83 @@
 #Aqui se definen y se guardan las rutas estandar de la pagina (a donde los usuarios pueden ir)
+import os
 import sqlite3 as sql
-from flask import Blueprint, render_template, request, flash  #Estos importes permiten tener este archivo como un plano para la pagina (Blueprint), a renderizar las vistas html
-                                                              #A pedir informacion (request) y mostrar mensajes en la pantalla (flash)
+import datetime
+from sqlalchemy.sql import text
+from sqlalchemy.sql.expression import desc
+from acinoyx_jubatus import db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from .__init__ import *
+from flask import Blueprint, render_template, request, flash, redirect, current_app # Estos importes permiten tener este archivo como un plano para la pagina (Blueprint), a renderizar las vistas html
+from flask.helpers import url_for                                                   # a pedir informacion (request) y mostrar mensajes en la pantalla (flash)
+from flask_login import login_required, current_user    # El metodo de current_user detectara si el usuario se encuentra logeado o no
 
 views = Blueprint('views', __name__)    #Asi es como se definira el "plano" (Blueprint)
+basedir = os.path.abspath(os.path.dirname(__file__))
+""" engine = create_engine("sqlite:///database.db")
+session = sessionmaker(bind=engine)()
+
+Base = declarative_base() """
+
+
+
+# Desde aqui empiezan las limitaciones a vistas del usuario.
+# Es necesario que el usuario se encuentre logeado para poder entrar
+# a estas URL's de la pagina
 
 @views.route('/', methods=['GET', 'POST'])
-def login():
-    return render_template("login.html")
+@login_required
+def feed():  
+    result = db.engine.execute(
+            """
+        SELECT photo, foot_note, post_date, photo_user_name
+        FROM post
+        ORDER
+        BY id DESC
+            """
+        )
 
-@views.route('/feed', methods=['GET', 'POST'])
-def feed():
-    return render_template("feed.html")
+    return render_template("feed.html", posts=result, user=current_user)
+    
 
 @views.route('/resultados_busqueda', methods=['GET', 'POST'])
+@login_required
 def resultados_busqueda():
-    return render_template("resultados_busqueda.html")
-
-@views.route('/registro', methods=['GET', 'POST'])
-def registro():
-
-    if request.method == 'POST':
-        name = request.form['nombre']
-        email = request.form['correo']
-        age = request.form['edad']
-        password = request.form['Contraseña']
-
-        print(name,email,age,password)
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-            cur.execute("SELECT email FROM users WHERE email = '{}'".format(email))
-
-            data = cur.fetchone()
-            if data :
-                flash("este correo ya esta registrado")
-            else:
-                cur.execute('INSERT INTO users (user_name, email, age, password) VALUES (?,?,?,?)',(name,email,age, password))
-                con.commit()
-                flash("registro exitoso")
-                return render_template("feed.html")
-
-
-
-    return render_template("registro.html")
-
+    return render_template("resultados_busqueda.html", user=current_user)
 
 @views.route('/dashboard_administrativo', methods=['GET', 'POST'])
+@login_required
 def dashboard_administrativo():
-    return render_template("dashboard_administrativo.html")
+    return render_template("dashboard_administrativo.html", user=current_user)
 
 @views.route('/detalle_post_general', methods=['GET', 'POST'])
+@login_required
 def detalle_post_general():
-    return render_template("detalle_post_general.html")
+    return render_template("detalle_post_general.html", user=current_user)
 
 @views.route('/detalle_post_2', methods=['GET', 'POST'])
+@login_required
 def detalle_post_2():
-    return render_template("detalle_post_2.html")
+    return render_template("detalle_post_2.html", user=current_user)
+
+@views.route('/post', methods=['GET', 'POST'])
+@login_required
+def post():
+    if request.method == 'POST':
+        user_id = request.cookies.get("user_id")
+        photo_user_name = request.cookies.get("photo_user_name")
+        post_title = request.form['title']
+        post_date = datetime.datetime.now()
+        upload_image = request.files['upload_image']   
+        if upload_image.filename != '':
+            filepath = os.path.join(basedir, current_app.config['UPLOAD_FOLDER'], upload_image.filename)
+            upload_image.save(filepath)
+        new_post = Post(photo=upload_image.filename, foot_note=post_title, post_date=post_date, user_id=user_id, photo_user_name=photo_user_name)   
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Publicacion exitosa!', category='succes')
+        return redirect(url_for('views.feed'))
+
+    return render_template("crear_post.html", user=current_user)
 
